b3a1249ff72585f6012b5873a401021e4f0738fb
John Watson <john@photodojo.org>
2012-10-17 11:19:41 -0400

    Added the MIT license.

--- pigman/pigman.py
+++ pigman/pigman.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python
-#
-# Created by John Watson on 2012-08-29.
-# Copyright (c) 2012 Eightyone Labs, Inc. All rights reserved.
-#
-
-
 from django.conf import settings
 from functools import wraps
 
